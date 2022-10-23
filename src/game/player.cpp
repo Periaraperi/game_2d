2ce4cf91712405922b2c8f8b611441c593949361
UNALST <ltskhvediani@vt.edu>
2022-10-23 16:55:00 -0400

    .

--- src/game/player.cpp
+++ src/game/player.cpp
@@ -6,11 +6,11 @@ Player::Player(const glm::vec2& start_pos, const std::unique_ptr<Texture>& sprit
 	:pos(start_pos), scale(glm::vec2(64.0f,64.0f)), 
 	scale_mat(glm::scale(glm::mat4(1.0f),glm::vec3(scale.x,scale.y,0.0f))), 
 	moving(false),
-	nframes(2), anim_duration(0.6f), anim_timer(0.0f), sprite_id(4)
+	nframes(2), anim_duration(0.6f), anim_timer(0.0f), sprite_id(0)
 {
 	calculate_model();
 	player_quad = std::make_unique<Quad>();
-	auto tex_coords = Renderer::instance()->get_tex_coords(sprite_id,0,32,32,sprite_sheet->get_width(),
+	auto tex_coords = Renderer::instance()->get_tex_coords(4,0,32,32,sprite_sheet->get_width(),
 																	 sprite_sheet->get_height());
 	auto qverts = gen_quad(tex_coords);
 	player_quad->modify_buffer(qverts);
@@ -27,6 +27,8 @@ void Player::update_pos(const glm::vec2& p)
 	calculate_model();
 }
 
+#include <iostream>
+
 void Player::animate(const glm::vec2& dir, const std::unique_ptr<Texture>& sprite_sheet)
 {
 	float time_per_frame = anim_duration/nframes;
@@ -39,6 +41,7 @@ void Player::animate(const glm::vec2& dir, const std::unique_ptr<Texture>& sprit
 	}
 
 	std::array<glm::vec2,4> tex_coords;
+
 	if (dir==glm::vec2(1.0f,0.0f)) { // right
 		tex_coords = Renderer::instance()->get_tex_coords(sprite_id,0,32,32,
 			sprite_sheet->get_width(),
